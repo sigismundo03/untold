d17bceb3bc9d47f23f8e49a6e6758cba8fd78a03
sigismundo03 <sigismundo4321@gmail.com>
2025-05-15 10:11:00 -0300

    feat: novas rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'routing/app_routes.dart';
 import 'ui/core/di/injection.dart';
 import 'ui/core/theme/theme.dart';
-import 'ui/login/screen/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    
-  );
+  await Firebase.initializeApp();
 
-setupDependencies();
+  setupDependencies();
   runApp(const MyApp());
 }
 
@@ -25,7 +23,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: AppTheme.themeData,
-      home: const LoginScreen(),
+      initialRoute: AppRoutes.home,
+      onGenerateRoute: AppRoutes.generateRoute,
     );
   }
 }
